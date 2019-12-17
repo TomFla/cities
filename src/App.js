93d1a24b1b6733ea9e725fffdb95760044583204
Tomasz Flakus <tomasz.flakus@gmail.com>
2019-12-17 12:53:37 +0100

    starting and add component.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from './logo.svg';
+import "./App.css";
 
 function App() {
-    return ( <
-        div className = "App" >
-
-        <
-        /div>
-    );
+  return (
+    <div className="App">
+      <input type="text/text" placeholder="type city" />
+      <button type="submit">Start</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./content.css";
+
+// function Content() {
+//   return <h1> Tu będzie wynik z zapytania </h1>;
+// }
+// export default Content;
