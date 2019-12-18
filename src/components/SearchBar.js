69b5d5031e7e648af3ca5cc79e564d2fd9c6fb48
Tomasz Flakus <tomasz.flakus@gmail.com>
2019-12-18 18:27:52 +0100

    adding logic into searchBar.js - trying to send Get request into the API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./SearchBar.css";
 
 class SearchBar extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    let city = document.getElementById("input");
+    console.log(city);
+  };
+
   render() {
     return (
       <div className="inputField">
-        <form className="formTag">
-          <input type="text" placeholder="type city" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Check 10 most polluted cities" />
           <button type="submit">Start</button>
         </form>
       </div>
